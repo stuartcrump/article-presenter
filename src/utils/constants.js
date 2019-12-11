b5231427790f9da4580521984e46b8ec62379a0a
stuartcrump <56249325+stuartcrump@users.noreply.github.com>
2019-12-11 11:09:53 +0000

    Update constants.js
    Added support to limit the contentType used to retrieve the articles.  Previously could have retrieved articles with undefined element values.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ import { wchGetHubInfoFromBaseURL } from '@ibm-wch-sdk/utils';
 const appName = 'acoustic-example'; // Enter your app name. 
 const taxonomy = `ForReactApp`; // Enter your taxonomy name.
 const headlineContentType = 'ReactAppHeadlines'; // Enter your Headline content type name.
+const contentType = 'ReactAppArticles';  // Enter the Article content Type.
 
 const developmentHubInfo = {
   apiUrl: { href: 'https://my10.digitalexperience.ibm.com/api/02df85f2-b4e9-4699-90a2-5c322475b2df/' },
@@ -22,4 +23,4 @@ console.log('hubinfo: ', hubInfo);
 console.log('taxonomy: ', taxonomy);
 console.log('headline content type:', headlineContentType);
 
-export { apiUrl, resourceUrl, taxonomy, asJSON, headlineContentType, pathName, appPath };
+export { apiUrl, resourceUrl, taxonomy, contentType, asJSON, headlineContentType, pathName, appPath };
